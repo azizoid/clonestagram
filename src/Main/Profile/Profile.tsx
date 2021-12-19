d82806c59219c7425d91a934d4131b602b96307c
azizoid <shaqhuseynov@gmail.com>
2021-12-19 20:18:05 +0100

    test: add render test for Profile

--- src/Main/Profile/Profile.tsx
+++ src/Main/Profile/Profile.tsx
@@ -4,23 +4,33 @@ import { Col, Card, Row, Image } from 'react-bootstrap';
 
 import styles from './Profile.module.scss';
 
-export const Profile = () => (
+export type ProfileProps = {
+    username: string;
+    fullName: string;
+    image: string;
+};
+
+export const Profile = ({
+    username,
+    fullName,
+    image,
+}: ProfileProps): JSX.Element => (
     <Col md={{ span: 3 }} className={styles.profile}>
         <Card body={true} bg="light" border="light">
             <Row className="align-items-center">
                 <Col md={{ span: 3 }}>
                     <Image
                         className="w-56"
-                        src="https://picsum.photos/id/473/56/56"
+                        src={image}
                         roundedCircle={true}
                         thumbnail={true}
                     />
                 </Col>
                 <Col md={{ span: 6 }}>
                     <Card.Link className={styles.profileLink}>
-                        azizoid
+                        {username}
                     </Card.Link>
-                    <Card.Text>Aziz Shahhuseynov</Card.Text>
+                    <Card.Text>{fullName}</Card.Text>
                 </Col>
                 <Col md={{ span: 3 }}>
                     <Card.Link className={styles.profileLink}>Switch</Card.Link>
