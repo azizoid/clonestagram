6ab87a3f3aaace13704c3def59bcb95afc7adea2
azizoid <shaqhuseynov@gmail.com>
2021-12-19 20:59:11 +0100

    feat: Feed header elements

--- src/Main/Feed/Feed.tsx
+++ src/Main/Feed/Feed.tsx
@@ -1,14 +1,35 @@
+import classnames from 'classnames';
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
+import { BsThreeDots } from 'react-icons/bs';
+
+import styles from './Feed.module.scss';
 
 export const Feed = () => {
     return (
-        <Col md={{ offset: 2, span: 6 }}>
-            <Card>
-                <Card.Header>Header</Card.Header>
-                <Card.Body>Body</Card.Body>
-                <Card.Footer>Footer</Card.Footer>
-            </Card>
-        </Col>
+        <Card className={styles.feed}>
+            <Card.Header className={styles.header}>
+                <div>
+                    <Image
+                        className="w-32"
+                        src="https://picsum.photos/id/101/32/32"
+                        roundedCircle={true}
+                        thumbnail={true}
+                    />
+                    <Card.Link
+                        className={classnames(
+                            'profileUsernameLink',
+                            styles.username,
+                        )}>
+                        azizoid
+                    </Card.Link>
+                </div>
+                <Card.Link className={styles.threeDots}>
+                    <BsThreeDots />
+                </Card.Link>
+            </Card.Header>
+            <Card.Body>Body</Card.Body>
+            <Card.Body>Footer</Card.Body>
+        </Card>
     );
 };
