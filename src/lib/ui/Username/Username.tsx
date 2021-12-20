75996a5e80173547b2d85c396a301dc8e9a12ef5
azizoid <shaqhuseynov@gmail.com>
2021-12-20 23:44:03 +0100

    ui: add classname to Username component

--- src/lib/ui/Username/Username.tsx
+++ src/lib/ui/Username/Username.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import styles from './Username.module.scss';
 
 export type UsernameProps = {
     username: string;
+    className?: string;
 };
 
-export const Username = ({ username }: UsernameProps): JSX.Element => (
-    <a className={styles.username}>{username}</a>
+export const Username = ({
+    username,
+    className,
+}: UsernameProps): JSX.Element => (
+    <a href="/#username" className={classnames(styles.username, className)}>
+        {username}
+    </a>
 );
