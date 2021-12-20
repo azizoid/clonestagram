b0603b8a158312ee44d1c8a2df62acf47e72d101
azizoid <shaqhuseynov@gmail.com>
2021-12-20 14:35:16 +0100

    feat: add image, comments and likes bar to Feed Component

--- src/Main/Feed/Feed.tsx
+++ src/Main/Feed/Feed.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Row, Image } from 'react-bootstrap';
 import { BsHeart, BsChatRight, BsTelegram, BsBookmark } from 'react-icons/bs';
 
 import styles from './Feed.module.scss';
@@ -14,16 +14,27 @@ export const Feed = () => {
                 className="w-614"
                 src="https://picsum.photos/id/101/614/614"
             />
-            <Card.Body className={styles.reactionsBar}>
-                <div>
-                    <BsHeart />
-                    <BsChatRight />
-                    <BsTelegram />
+            <Card.Body>
+                <div className={styles.reactionsBar}>
+                    <div>
+                        <BsHeart />
+                        <BsChatRight />
+                        <BsTelegram />
+                    </div>
+                    <BsBookmark />
                 </div>
-                <BsBookmark />
+                <div className={styles.commentsAndLikesBar}>
+                    <Card.Text className={styles.likes}>20 likes</Card.Text>
+                    <Card.Text className={styles.title}>
+                        <b>Username</b> Photo Title
+                    </Card.Text>
+                    <Card.Text>
+                        <small>19 hours ago</small>
+                    </Card.Text>
+                </div>
             </Card.Body>
 
-            <Card.Body>Footer</Card.Body>
+            <Card.Footer>Footer</Card.Footer>
         </Card>
     );
 };
